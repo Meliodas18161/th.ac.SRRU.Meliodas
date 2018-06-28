b3bfe0f79ce946dc7683c466a16c99e7d339c6bf
meliodas18161@gmail.com <meliodas18161@gmail.com>
2018-06-28 12:12:22 +0700

    Updated  Day 2

--- app/src/main/java/meliodas/srru/ac/th/srrufriend/fragment/RegisterFeagment.java
+++ app/src/main/java/meliodas/srru/ac/th/srrufriend/fragment/RegisterFeagment.java
@@ -8,24 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.zip.Inflater;
-
 import meliodas.srru.ac.th.srrufriend.R;
 
-public class MainFragment extends Fragment{
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-//        Register Controller
-
-    }   // Main Method
+public class RegisterFeagment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R. layout. fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
-}   // Main Class
+}
